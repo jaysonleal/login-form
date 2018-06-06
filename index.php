1251e7c044330de829fa51542bf45e3c3a458041
jaysonleal <jaysonleal.jl@gmail.com>
2018-06-06 20:52:27 +0800

    First commit in login form

---
+++ index.php
@@ -0,0 +1,22 @@
+<link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" rel="stylesheet">
+<link rel="stylesheet" type="text/css" href="particle.css">
+
+<div id="particles-js">
+	<div class="container">
+		<div id="login-box">
+			<div class="logo">
+				<img src="user-pic.png" class="img img-responsive img-circle center-block"/> <!--add link of picture-->
+				<h1 class="logo-caption"><span class="tweak">L</span>ogin</h1>
+			</div><!-- /.logo -->
+			<div class="controls">
+				<input type="text" name="username" placeholder="Username" class="form-control" />
+				<input type="text" name="username" placeholder="Password" class="form-control" />
+				<button type="button" class="btn btn-default btn-block btn-custom">Login</button>
+			</div><!-- /.controls -->
+		</div><!-- /#login-box -->
+	</div><!-- /.container -->
+</div>
+<script src="https://code.jquery.com/jquery-1.12.4.js"></script>
+
+<script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/particles.js/2.0.0/particles.min.js"></script>
+<script type="text/javascript" src="particle.js"></script>
